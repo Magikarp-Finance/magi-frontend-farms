2f1551a1bb769dcaf4d40420e7d1cc1e1836a14f
sub722 <jhswebdeb@gmail.com>
2021-03-17 14:49:51 +0100

    first commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xf87b5c7f7061fc261f176f23683a67d8da44aef3',
+    97: '',
+  },
+  masterChef: {
+    56: '0xA15E3b5c891529D52f99520B712E7595EE04A0F8',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
