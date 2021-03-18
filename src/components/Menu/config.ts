88ebce56286b4e1adf8c27a3ce4f5cde65415195
sub722 <jhswebdeb@gmail.com>
2021-03-18 17:33:27 +0100

    edit Menus

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -11,12 +11,16 @@ const config: MenuEntry[] = [
     icon: 'TradeIcon',
     items: [
       {
-        label: 'Exchange',
-        href: 'https://exchange.goosedefi.com/',
+        label: 'Exchange Magikarp',
+        href: 'https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xc603b1fba6A5b502Be4f1Ab27591bF4e75A3460c',
+      },
+      {
+        label: 'Exchange Gyarados',
+        href: '#',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.goosedefi.com/#/pool',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
         
       },
       
@@ -54,27 +58,27 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        href: '#',
       },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
+  // {
+  //    label: 'CoinGecko',
+  //    href: 'https://www.coingecko.com/en/coins/goose-finance',
+  //  },
+  //  {
+  //    label: 'CoinMarketCap',
+  //    href: 'https://coinmarketcap.com/currencies/goose-finance/',
+  //  },
+  //  {
+  //    label: 'AstroTools',
+  //    href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+  //  },
     ],
   },
   {
     label: 'More',
     icon: 'MoreIcon',
     items: [
-      {
+     {
         label: 'Github',
         href: 'https://github.com/goosedefi/',
         
@@ -88,19 +92,19 @@ const config: MenuEntry[] = [
         href: 'https://goosefinance.medium.com/',
       },
     ],
-  },
+  }, 
+  // {
+  //  label: 'Partnerships/IFO',
+  //  icon: 'GooseIcon',
+  //  href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  // },
   {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
-  },
-  {
-    label: 'Audit by Hacken',
+    label: 'Audit by Techrate (MAGI)',
     icon: 'AuditIcon',
     href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
   },
   {
-    label: 'Audit by CertiK',
+    label: 'Audit by CertiK (GYARADOS)',
     icon: 'AuditIcon',
     href: 'https://certik.org/projects/goose-finance',
   },
