00764d73010f0ae25a8863ed96d5bade3ced4241
sub722 <jhswebdeb@gmail.com>
2021-03-19 19:17:09 +0100

    change GYA for gya

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -127,7 +127,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
 
   return (
     <FCard>
-      {(farm.tokenSymbol === 'GYA' || farm.tokenSymbol === 'MAGI' ) && <StyledCardAccent />}
+      {(farm.tokenSymbol === 'gya' || farm.tokenSymbol === 'MAGI' ) && <StyledCardAccent />}
       <CardHeading
         lpLabel={lpLabel}
         multiplier={farm.multiplier}
