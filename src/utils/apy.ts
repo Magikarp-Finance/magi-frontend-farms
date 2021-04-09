c0412e9439468859f0b02601eb4888e46443f83c
sub722 <jhswebdeb@gmail.com>
2021-04-09 13:57:40 +0200

    fixed apr

--- src/utils/apy.ts
+++ src/utils/apy.ts
@@ -18,7 +18,7 @@ export const getPoolApy = (
 	const rewardTokenPrice = stakingTokenPrice * 0.6
 	const totalRewardPricePerYear = new BigNumber(rewardTokenPrice).times(tokenPerBlock).times(BLOCKS_PER_YEAR)
 	const totalStakingTokenInPool = new BigNumber(stakingTokenPrice).times(totalStaked)
-	const apy = totalRewardPricePerYear.div(totalStakingTokenInPool).times(10)
+	const apy = totalRewardPricePerYear.div(totalStakingTokenInPool).times(100)
 	/*
 	console.log('staking token price', stakingTokenPrice)
 	console.log('total Reward Price Per Year', totalRewardPricePerYear.toFixed(2))
